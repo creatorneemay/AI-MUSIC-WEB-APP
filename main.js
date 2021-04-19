c16e38599f659812f1ab1010ab3616814d438b28
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2021-04-19 10:19:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ lwristx="";
 rwristx="";
 lwristy="";
 rwristy="";
+lwrists="";
+rwrists="";
 function preload(){
     hp=loadSound("music.mp3")
+    pp=loadSound("music2.mp3")
 }
 function setup(){
     canvas=createCanvas(400,400);
@@ -18,12 +21,13 @@ function setup(){
 function modelloaded(){
     console.log("Model Loaded");
 }
-function draw(){
-    image(video,0,0,400,400)
+function play(){
+    hp.play();
+    pp.stop()
 }
 function pause(){
-    hp.pause();
-    pp.pause();
+    pp.play()
+    hp.stop()
 }
 function gotposes(results){
     if(results.length>0){
@@ -32,6 +36,50 @@ function gotposes(results){
         lwristy=results[0].pose.leftWrist.y;
         rwristx=results[0].pose.rightWrist.x;
         rwristy=results[0].pose.rightWrist.y;
+        lwrists=results[0].pose.keypoints[9].score;
+        rwrists=results[0].pose.keypoints[10].score;
         console.log(lwristx,lwristy,rwristx,rwristy);
+    }
+}
+function draw(){
+    image(video,0,0,400,400)
+    fill("red");
+    stroke("blue");
+    if(lwrists>0.2){
+        circle(lwristx,lwristy,20);
+        lwristy_number=Number(lwristy);
+        remd=floor(lwristy);
+        volume=remd/500;
+        hp.setVolume(volume);
+        pp.setVolume(volume);
+        document.getElementById("pp").innerHTML="volume= "+ volume;
+    }
+    if(rwrists>0.2){
+        circle(rwristx,rwristy,20);
+        if(rwristy>0&&rwristy<=100){
+            hp.rate(0.5)
+            pp.rate(0.5)
+            document.getElementById("hp").innerHTML="speed= 0.5x";
+        }
+        else if(rwristy>100&&rwristy<=200){
+            song.rate(1)
+            pp.rate(1)
+            document.getElementById("hp").innerHTML="speed= 1x";
+        }
+        else if(rwristy>200&&rwristy<=300){
+            song.rate(1.5)
+            pp.rate(1.5)
+            document.getElementById("hp").innerHTML="speed= 1.5x";
+        }
+        else if(rwristy>300&&rwristy<=400){
+            song.rate(2)
+            pp.rate(2)
+            document.getElementById("hp").innerHTML="speed= 2x";
+        }
+        else if(rwristy>400&&rwristy<=500){
+            song.rate(2.5)
+            pp.rate(2.5)
+            document.getElementById("hp").innerHTML="speed= 2.5x";
+        }
     }
 }
